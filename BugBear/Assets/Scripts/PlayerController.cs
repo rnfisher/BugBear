a697a8f28ab7c388a33ff315e14226c76f57d10d
rnfisher <rnf.fisher@gmail.com>
2020-10-12 00:25:42 -0400

    art
    added collectable and enemy prefabs to level 1, added move animation to player

--- BugBear/Assets/Scripts/PlayerController.cs
+++ BugBear/Assets/Scripts/PlayerController.cs
@@ -30,7 +30,7 @@ public class PlayerController : MonoBehaviour
        if (Input.GetButton("Fire1") && Time.time > nextFire) //fireRate value adjusts shots per second
         {
             nextFire = Time.time + fireRate; 
-            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);  //instantiates a Shot in front of the player on button press
+            Instantiate(shot, shotSpawn.position, Quaternion.identity);  //instantiates a Shot in front of the player on button press
         }
     }
 
