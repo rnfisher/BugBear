7ea5a36c49dc389c8b80d7a7dbf2614027d78f24
rnfisher <rnf.fisher@gmail.com>
2020-10-12 01:12:57 -0400

    Food and Enemy spawn

---
+++ BugBear/Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class EnemySpawn : MonoBehaviour
+{
+    public GameObject enemy;
+    public Transform enemySpawnPos;
+    public float spawnRate;
+
+    public float negValue;
+    public float posValue;
+
+    private float nextSpawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > nextSpawn)
+        {
+            nextSpawn = Time.time + spawnRate;
+            Vector3 position = new Vector3(enemySpawnPos.position.x + Random.Range(negValue, posValue), 0, enemySpawnPos.position.z);
+            Instantiate(enemy, position, Quaternion.identity);
+        }
+    }
+}
---
+++ BugBear/Assets/Scripts/FoodSpawn.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class FoodSpawn : MonoBehaviour
+{
+    public GameObject food;
+    public Transform foodSpawnPos;
+    public float spawnRate;
+
+    public float negValue;
+    public float posValue;
+
+    private float nextSpawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > nextSpawn)
+        {
+            nextSpawn = Time.time + spawnRate;
+            Vector3 position = new Vector3(foodSpawnPos.position.x + Random.Range(negValue, posValue), 0, foodSpawnPos.position.z);
+            Instantiate(food, position, Quaternion.identity);
+        }
+    }
+}
