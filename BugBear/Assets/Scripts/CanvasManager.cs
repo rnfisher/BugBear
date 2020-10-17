cbb7311e650778ff929271155822352fe0b41c89
dwiddel <47085084+dwiddel@users.noreply.github.com>
2020-10-17 16:32:34 -0400

    UI Updates

---
+++ BugBear/Assets/Scripts/CanvasManager.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Player
+{
+    public class CanvasManager : MonoBehaviour
+    {
+        public static CanvasManager instance;
+        public GameObject pauseMenu;
+        public GameObject optionsMenu;
+        public GameObject healthBar;
+        public GameObject joystick;
+        public Button pauseBtn;
+        public Button fireBtn;
+        [HideInInspector] private string currentScene;
+        [HideInInspector] public static string nextScene;
+        [HideInInspector] public bool GameIsPaused;
+
+        private void Awake()
+        {
+            instance = this;
+        }
+
+        void Start()
+        {
+            GameIsPaused = false;
+            //UnlockScreen();
+            Screen.SetResolution(1920, 1080, true);
+            currentScene = SceneManager.GetActiveScene().name;
+            pauseBtn.SetActive(false);
+        }
+
+        void Update()
+        {
+            /*if (Input.GetKeyDown("space"))
+            {
+                print("space key was pressed");
+            }*/
+        }
+
+        public void LockScreen()
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            Time.timeScale = 0f;
+
+        }
+
+        public void UnlockScreen()
+        {
+            //Cursor.lockState = CursorLockMode.Locked;
+            //Cursor.visible = false;
+            Time.timeScale = 1f;
+        }
+
+        public void Pause()
+        {
+            pauseMenu.SetActive(true);
+            LockScreen();
+        }
+
+        public void Resume()
+        {
+            GameIsPaused = false;
+            pauseMenu.SetActive(false);
+            UnlockScreen();
+        }
+
+        public void Fire()
+        {
+            PlayerController.instance.FireWeapon();
+        }
+
+        public void OptionsMenu()
+        {
+            pauseMenu.SetActive(false);
+            optionsMenu.SetActive(true);
+        }
+
+        public void Back()
+        {
+            pauseMenu.SetActive(true);
+            optionsMenu.SetActive(false);
+        }
+
+        // Load Scenes by name
+        public void LoadSceneByName(string sceneName)
+        {
+            SceneManager.LoadScene(sceneName);
+            Time.timeScale = 1f;
+            SceneSettings(sceneName);
+        }
+
+        public void SceneSettings(string s)
+        {
+            switch (s)
+            {
+                case "Home":
+                    Cursor.lockState = CursorLockMode.None;
+                    Cursor.visible = true;
+                    break;
+                case "Level 1":
+
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
+
--- BugBear/Assets/Scripts/PlayerController.cs
+++ BugBear/Assets/Scripts/PlayerController.cs
@@ -5,53 +5,64 @@ using System.IO;
 using UnityEngine;
 using UnityEngine.Experimental.XR;
 
-[System.Serializable]
-public class Boundary //This is the section where you would set the boundary in the scene
+namespace Player
 {
-    public float xMin, xMax, zMin, zMax; //These are the values you will set for the boundary in the Inspector Pane
-}
-
-public class PlayerController : MonoBehaviour
-{
-    public float speed;
-    public Boundary boundary;
-    public float bugbearSpeed;
-
-    public GameObject shot;
-    public Transform shotSpawn;
-    public float fireRate;
-
-    public VirtualJoystick moveJoystick;
-
-    private float nextFire;
-
-    void Update()
+    [System.Serializable]
+    public class Boundary //This is the section where you would set the boundary in the scene
     {
-       if (Input.GetButton("Fire1") && Time.time > nextFire) //fireRate value adjusts shots per second
-        {
-            nextFire = Time.time + fireRate; 
-            Instantiate(shot, shotSpawn.position, Quaternion.identity);  //instantiates a Shot in front of the player on button press
-        }
+        public float xMin, xMax, zMin, zMax; //These are the values you will set for the boundary in the Inspector Pane
     }
 
-    void FixedUpdate()
+    public class PlayerController : MonoBehaviour
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        public static PlayerController instance;
+        public float speed;
+        public Boundary boundary;
+        public GameObject shot;
+        public Transform shotSpawn;
+        public float fireRate;
+        public VirtualJoystick moveJoystick;
+        private float nextFire;
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        GetComponent<Rigidbody>().velocity = movement * speed;
-
-        GetComponent<Rigidbody>().position = new Vector3
-        (
-            Mathf.Clamp(GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax),
-            0.0f,
-            Mathf.Clamp(GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
-        );
-
-        if(moveJoystick.InputDirection != Vector3.zero)
+        private void Awake()
         {
-            GetComponent<Rigidbody>().velocity = moveJoystick.InputDirection * bugbearSpeed;   
+            instance = this;
+        }
+
+        void Update()
+        {
+
+        }
+
+        void FixedUpdate()
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+
+            Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+            GetComponent<Rigidbody>().velocity = movement * speed;
+
+            GetComponent<Rigidbody>().position = new Vector3
+            (
+                Mathf.Clamp(GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax),
+                0.0f,
+                Mathf.Clamp(GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
+            );
+
+            if (moveJoystick.InputDirection != Vector3.zero)
+            {
+                GetComponent<Rigidbody>().velocity = moveJoystick.InputDirection * speed;
+            }
+        }
+
+        public void FireWeapon()
+        {
+            if (Time.time > nextFire) //fireRate value adjusts shots per second
+            {
+                nextFire = Time.time + fireRate;
+                Instantiate(shot, shotSpawn.position, Quaternion.identity);  //instantiates a Shot in front of the player on button press
+            }
         }
     }
 }
+
