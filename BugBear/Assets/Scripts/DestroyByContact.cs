86dfbaa13072e57a3425c74e7a55a4f5959c2434
NsuarKnights <Nicholas.Suarez001@knights.ucf.edu>
2020-10-19 17:42:21 -0400

    Nick's changes, 10/19
    Copied Level1 Scene into Nick's folder for testing purposes.
    
    Added and applied tags for Enemy and Collectible.
    
    Created and applied script to Enemy prefab that destroys only the player upon contact. This does not reflect how it will play in the final version if the game, but will suffice as a placeholder.

---
+++ BugBear/Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Boundary")
+        {
+            return;
+        }
+        else if (other.tag == "Collectible")
+        {
+            return;
+        }
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
