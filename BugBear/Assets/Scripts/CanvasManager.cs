83e3e975b73d808f3abe411e32b99b72d92a8564
Rboysel <54560092+Rboysel@users.noreply.github.com>
2020-10-20 18:02:11 -0400

    Added Basic Score UI

--- BugBear/Assets/Scripts/CanvasManager.cs
+++ BugBear/Assets/Scripts/CanvasManager.cs
@@ -14,9 +14,11 @@ namespace Player
         public GameObject joystick;
         public Button pauseBtn;
         public Button fireBtn;
+        public Text ScoreText;
         [HideInInspector] private string currentScene;
         [HideInInspector] public static string nextScene;
         [HideInInspector] public bool GameIsPaused;
+        private int score;
 
         private void Awake()
         {
@@ -29,6 +31,8 @@ namespace Player
             //UnlockScreen();
             //Screen.SetResolution(1920, 1080, true);
             currentScene = SceneManager.GetActiveScene().name;
+            score = 0;
+            UpdateScore();
         }
 
         void Update()
@@ -106,6 +110,15 @@ namespace Player
                 default:
                     break;
             }
+        }
+        public void AddScore(int newScoreValue)
+        {
+            score += newScoreValue;
+            UpdateScore();
+        }
+        void UpdateScore()
+        {
+            ScoreText.text = "Score: " + score;
         }
     }
 }
