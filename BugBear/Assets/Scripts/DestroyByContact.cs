f3846b9507b86ddb5f7efd0286a2abbd98fbb832
NsuarKnights <Nicholas.Suarez001@knights.ucf.edu>
2020-10-24 17:07:26 -0400

    Push 10/24
    Added Wave System for enemy spawning.
    
    Added scoring, but needs fixing
    
    Addeed Sound, it is very obviously placeholder.

--- BugBear/Assets/Scripts/DestroyByContact.cs
+++ BugBear/Assets/Scripts/DestroyByContact.cs
@@ -4,17 +4,33 @@ using UnityEngine;
 
 public class DestroyByContact : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
+
+    public int scoreValue;
+    private GameController gameController;
+
+    void Start()
+    {
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController"); //This section is to detect if the object that collides with this script also has the 'GameController' script
+        if (gameController != null)                                                 //And apply the values associated with that script.
+        {
+            gameController = gameControllerObject.GetComponent<GameController>();
+        }
+        if (gameController == null)
+        {
+            Debug.Log("Cannot find 'GameController' script");
+        }
+    }
+    void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Boundary")
         {
             return;
         }
-        else if (other.tag == "Collectible")
+        else if (other.tag == "Enemy")
         {
             return;
         }
-        else if (other.tag == "Projectile")
+        else if (other.tag == "Collectible")
         {
             return;
         }
@@ -24,5 +40,7 @@ public class DestroyByContact : MonoBehaviour
         }
         Destroy(other.gameObject);
         Destroy(gameObject);
+        gameController.AddScore(scoreValue); //add score when destroyed
+        
     }
 }
