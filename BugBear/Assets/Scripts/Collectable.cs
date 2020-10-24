f97e2219035e90665a9b8a3e320f044c0170a6e3
dwiddel <47085084+dwiddel@users.noreply.github.com>
2020-10-24 16:02:03 -0400

    Collectables
    Scoring is working for collectables

---
+++ BugBear/Assets/Scripts/Collectable.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player
+{
+    public class Collectable : MonoBehaviour
+    {
+        private string tagName;
+        [SerializeField] private Animator anim;
+        public static Collectable instance;
+
+        private void Awake()
+        {
+            instance = this;
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            tagName = this.gameObject.tag;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.tag == "Player")
+            {
+                //anim.SetTrigger("");
+                CanvasManager.instance.AddScore(1);
+                Destroy(gameObject);
+            }
+        }
+
+        private void IncreaseHealth()
+        {
+
+        }
+    }
+}
+
