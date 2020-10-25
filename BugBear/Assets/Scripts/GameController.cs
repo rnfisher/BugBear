f0cb11f391f09f38cf7643a0b2b2de4fb3b0e7e2
NsuarKnights <Nicholas.Suarez001@knights.ucf.edu>
2020-10-24 21:20:29 -0400

    Moved Food Spawning to GameController

--- BugBear/Assets/Scripts/GameController.cs
+++ BugBear/Assets/Scripts/GameController.cs
@@ -5,12 +5,18 @@ using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
-    public GameObject hazard;
-    public Vector3 spawnValues;
-    public int hazardCount;
-    public float spawnWait;
-    public float startWait;
-    public float waveWait;
+    public GameObject enemy;
+    public GameObject food;
+    public Vector3 enemySpawnValues;
+    public Vector3 foodSpawnValues;
+    public int enemyCount;
+    public int foodCount;
+    public float enemySpawnWait;
+    public float enemyStartWait;
+    public float enemyWaveWait;
+    public float foodSpawnWait;
+    public float foodStartWait;
+    public float foodWaveWait;
 
     public Text scoreText;
     private int score;
@@ -24,17 +30,26 @@ public class GameController : MonoBehaviour
 
     IEnumerator SpawnWaves()
     {
-        yield return new WaitForSeconds(startWait);
+        yield return new WaitForSeconds(enemyStartWait);
         while (true)
         {
-            for (int i = 0; i < hazardCount; i++)
+            for (int i = 0, j = 0; i < enemyCount; i++,j++)
             {
-                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z); //For 'x' the script will chose random numbers between x and -x
+                Vector3 spawnPosition = new Vector3(Random.Range(-enemySpawnValues.x, enemySpawnValues.x), enemySpawnValues.y, enemySpawnValues.z); //For 'x' the script will chose random numbers between x and -x
                 Quaternion spawnRotation = Quaternion.identity;
-                Instantiate(hazard, spawnPosition, spawnRotation);
-                yield return new WaitForSeconds(spawnWait);
+                Instantiate(enemy, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(enemySpawnWait);
             }
-            yield return new WaitForSeconds(waveWait);
+            yield return new WaitForSeconds(enemyWaveWait);
+
+            for (int i = 0; i < foodCount; i++)
+            {
+                Vector3 spawnPosition = new Vector3(Random.Range(-foodSpawnValues.x, foodSpawnValues.x), foodSpawnValues.y, foodSpawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(food, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(foodSpawnWait);
+            }
+            yield return new WaitForSeconds(foodWaveWait);
         }
     }
 
