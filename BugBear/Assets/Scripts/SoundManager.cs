6b1b3acae3232e9ffec8f2f1d3711118c9ece285
dwiddel <47085084+dwiddel@users.noreply.github.com>
2020-11-08 15:44:41 -0500

    Sound Manager

---
+++ BugBear/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public Slider volumeSlider;
+    private float masterVolume = 1f;
+    private string currentScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentScene = SceneManager.GetActiveScene().name;
+        volumeSlider.onValueChanged.AddListener(delegate {
+            UpdateVolume();
+        });
+        masterVolume = PlayerPrefs.GetFloat("MasterVolume");
+        volumeSlider.value = masterVolume;
+        audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void StartVolume() { 
+    }
+
+    private void UpdateVolume()
+    {
+        masterVolume = volumeSlider.value;
+        audioSource.volume = masterVolume;
+        PlayerPrefs.SetFloat("MasterVolume", masterVolume);
+    }
+}
