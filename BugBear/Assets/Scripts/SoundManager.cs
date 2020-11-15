abbb0fe53d1ef021b2e279daa0eff7b2512ec410
dwiddel <47085084+dwiddel@users.noreply.github.com>
2020-11-15 14:53:56 -0500

    Sound Manager Update

--- BugBear/Assets/Scripts/SoundManager.cs
+++ BugBear/Assets/Scripts/SoundManager.cs
@@ -4,29 +4,47 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class SoundManager : MonoBehaviour
+namespace Player
 {
-    public AudioSource audioSource;
-    public Slider volumeSlider;
-    private float masterVolume = 1f;
-    private string currentScene;
-
-    // Start is called before the first frame update
-    void Start()
+    public class SoundManager : MonoBehaviour
     {
-        currentScene = SceneManager.GetActiveScene().name;
-        volumeSlider.onValueChanged.AddListener(delegate {
-            UpdateVolume();
-        });
-        masterVolume = PlayerPrefs.GetFloat("MasterVolume");
-        volumeSlider.value = masterVolume;
-        audioSource.Play();
+        public static SoundManager instance;
+        public AudioSource music;
+        public AudioSource shoot;
+        public AudioSource enemyDeath;
+        public AudioSource playerDeath;
+        public AudioSource foodCollection;
+        public AudioSource collision;
+        public Slider volumeSlider;
+        private float masterVolume = 1f;
+        private string currentScene;
+
+        private void Awake()
+        {
+            instance = this;
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            currentScene = SceneManager.GetActiveScene().name;
+            volumeSlider.onValueChanged.AddListener(delegate {
+                UpdateVolume();
+            });
+            masterVolume = PlayerPrefs.GetFloat("MasterVolume");
+            volumeSlider.value = masterVolume;
+            if (currentScene == "Level 1" || currentScene == "Level 2" || currentScene == "Level 3")
+            {
+                music.Play();
+            }
+        }
+
+        private void UpdateVolume()
+        {
+            masterVolume = volumeSlider.value;
+            music.volume = masterVolume;
+            PlayerPrefs.SetFloat("MasterVolume", masterVolume);
+        }
     }
 
-    private void UpdateVolume()
-    {
-        masterVolume = volumeSlider.value;
-        audioSource.volume = masterVolume;
-        PlayerPrefs.SetFloat("MasterVolume", masterVolume);
-    }
 }
